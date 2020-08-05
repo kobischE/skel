51de842e7b136fb244b50e235e626853e718dc5d
kobischE <ekobsch@gmail.com>
2020-08-05 17:31:15 +0200

    Dernière modifications

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,7 +7,7 @@ set -e
 git init
 git add .
 git commit -m "Initialisation du projet
-À partir du squelette : {{ cookiecutter.git_remote.replace(':', '/').replace('git@', 'https://') }}"
+À partir du squelette : {{ cookiecutter.git_remote_skel.replace(':', '/').replace('git@', 'https://') }}"
 git tag 0.0.0
 
 if {{ cookiecutter.github_create_repo }}; then
@@ -21,7 +21,7 @@ if {{ cookiecutter.github_create_repo }}; then
     curl \
         -X POST \
         -H 'Content-Type: application/json' \
-        --data '{"alias":"Skel","avatar":"https://i.imgur.com/adBSucj.jpg","text":"Un nouveau dépôt a été créé !","attachments":[{"title":"{{ cookiecutter.github_owner }}/{{ cookiecutter.project_slug }}","title_link":"https://github.com/{{ cookiecutter.github_owner }}/{{ cookiecutter.project_slug }}","text":"Project Github : https://github.com/{{ cookiecutter.github_owner }}/{{ cookiecutter.project_slug }}\nSquelette : {{ cookiecutter.git_remote.replace(':', '/').replace('git@', 'https://') }}" }]}' \
+        --data '{"alias":"Skel","avatar":"https://i.imgur.com/adBSucj.jpg","text":"Un nouveau dépôt a été créé !","attachments":[{"title":"{{ cookiecutter.github_owner }}/{{ cookiecutter.project_slug }}","title_link":"https://github.com/{{ cookiecutter.github_owner }}/{{ cookiecutter.project_slug }}","text":"Project Github : https://github.com/{{ cookiecutter.github_owner }}/{{ cookiecutter.project_slug }}\nSquelette : {{ cookiecutter.git_remote_skel.replace(':', '/').replace('git@', 'https://') }}" }]}' \
         {{ cookiecutter.rocketchat_hook_url }}
 fi
 
